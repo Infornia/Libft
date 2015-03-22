0d7fa39a9abfe1e3668966279a5a038185efdc51
Michael WILK <mwilk@e3r3p14.42.fr>
2015-03-22 20:15:31 +0100

    add tree functions

--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: mwilk <mwilk@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/04 14:27:02 by mwilk             #+#    #+#             */
-/*   Updated: 2015/03/22 19:33:59 by mwilk            ###   ########.fr       */
+/*   Updated: 2015/03/22 20:15:17 by mwilk            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,10 +36,12 @@ typedef struct		s_tree
 	struct	s_tree	*left;
 }					t_tree;
 
-t_tree	*ft_tree_add(t_tree *root, t_tree *node, int (*cmp)(void *, void *));
-void	ft_tree_del(t_tree **parent, void (*del)(void *, size_t));
-int		ft_tree_h(t_tree *node);
-t_tree	*ft_tree_new(const void *content, size_t content_size);
+t_tree	*tt_tree_add(t_tree *root, t_tree *node, int (*cmp)(void *, void *));
+void	tt_tree_del(t_tree **parent, void (*del)(void *, size_t));
+int		tt_tree_h(t_tree *node);
+t_tree	*tt_tree_new(const void *content, size_t content_size);
+void	tt_tree_iter_ord(t_tree **head, void (*f)(t_tree *node));
+void	tt_tree_iter_rev(t_tree **head, void (*f)(t_tree *node));
 
 /*
 ** LISTS
